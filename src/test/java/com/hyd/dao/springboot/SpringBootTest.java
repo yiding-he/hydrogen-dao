d7865391f9ac9255aa40f11e40bbe50b69db9fc8
yiding he <yiding.he@gmail.com>
2019-01-17 14:32:43 +0800

    Update spring boot auto-configuration documentations

--- src/test/java/com/hyd/dao/springboot/SpringBootTest.java
+++ src/test/java/com/hyd/dao/springboot/SpringBootTest.java
@@ -1,6 +1,7 @@
 package com.hyd.dao.springboot;
 
 import com.hyd.dao.DAO;
+import com.hyd.dao.DataSources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,16 +12,27 @@ import org.springframework.context.annotation.Bean;
 public class SpringBootTest {
 
     @Autowired
-    private DAO dao;
+    private DAO dao;            // hydrogen-dao.data-sources.default
+
+    @Autowired
+    private DAO anotherDao;     // hydrogen-dao.data-sources.another
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootTest.class, "--spring.profiles.active=h2");
     }
 
     @Bean
+    public DAO anotherDao(DataSources dataSources) {
+        return dataSources.getDAO("another");
+    }
+
+    @Bean
     public CommandLineRunner run() {
         return args -> {
+            System.out.println("dao tables:");
             dao.query("show tables").forEach(System.out::println);
+            System.out.println("anotherDao tables:");
+            anotherDao.query("show tables").forEach(System.out::println);
         };
     }
 }
