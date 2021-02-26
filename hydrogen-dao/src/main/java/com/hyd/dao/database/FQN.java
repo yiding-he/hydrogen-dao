5ce6583cdbd4175259cca4705cc7be4ac25e2147
Yiding He <yiding.he@gmail.com>
2021-02-26 18:18:16 +0800

    修复生成 POJO 代码失败的问题

--- hydrogen-dao/src/main/java/com/hyd/dao/database/FQN.java
+++ hydrogen-dao/src/main/java/com/hyd/dao/database/FQN.java
@@ -5,8 +5,6 @@ import com.hyd.dao.mate.util.Str;
 import lombok.EqualsAndHashCode;
 
 import java.sql.SQLException;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * FQN of a table.
@@ -15,9 +13,6 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 @EqualsAndHashCode
 public class FQN {
-
-    // Map<DataSourceName, Schema>
-    private static final Map<String, String> SCHEMA_CACHE = new ConcurrentHashMap<>();
 
     private final String schema;
 
@@ -30,26 +25,26 @@ public class FQN {
             throw new IllegalArgumentException("FQN parameter cannot be empty");
         }
 
-        String dataSourceName = context.getDataSourceName();
-        this.schema = SCHEMA_CACHE.computeIfAbsent(dataSourceName, _ds -> {
-            try {
-                // 如果表名带 "." 则取前面部分，否则从 Connection 对象中取，如果取不到则使用 "%"
-                if (fqn.contains(".")) {
-                    return Str.subStringBeforeLast(fqn, ".");
-                } else {
-                    return Str.fromCandidates(
-                        context.getConnection().getSchema(),
-                        context.getConnection().getCatalog(),
-                        "%"
-                    );
-                }
-            } catch (SQLException e) {
-                throw new DAOException(e);
-            }
-        });
-
+        this.schema = parseSchema(context, fqn);
         this.name = fqn.contains(".") ? Str.subStringAfterLast(fqn, ".") : fqn;
         this.quotedName = context.getDialect().quote(this.schema, this.name);
+    }
+
+    private String parseSchema(ConnectionContext context, String fqn) {
+        try {
+            // 如果表名带 "." 则取前面部分，否则从 Connection 对象中取，如果取不到则使用 "%"
+            if (fqn.contains(".")) {
+                return Str.subStringBeforeLast(fqn, ".");
+            } else {
+                return Str.fromCandidates(
+                    context.getConnection().getSchema(),
+                    context.getConnection().getCatalog(),
+                    "%"
+                );
+            }
+        } catch (SQLException e) {
+            throw new DAOException(e);
+        }
     }
 
     public String getSchema(String defaultValue) {
