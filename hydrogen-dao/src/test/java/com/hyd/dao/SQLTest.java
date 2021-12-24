90efb73eecc6c372fa8cefd186be45e7e62b283a
Yiding He <yiding.he@gmail.com>
2021-12-24 16:11:29 +0800

    允许SQL对象进行子查询嵌套

--- hydrogen-dao/src/test/java/com/hyd/dao/SQLTest.java
+++ hydrogen-dao/src/test/java/com/hyd/dao/SQLTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
+import static com.hyd.dao.SQL.Select;
+
 public class SQLTest {
 
     private static void output(SQL.Generatable<?> generatable) {
@@ -16,7 +18,7 @@ public class SQLTest {
     @Test
     public void testJoin() throws Exception {
         String name = null;
-        SQL.Select select = SQL.Select("*")
+        Select select = Select("*")
             .From("table1 t1")
             .InnerJoin("Table2 t2 on t1.id=t2.id+?", 10)
             .IfNotEmpty(name, (_select, _name) ->
@@ -26,25 +28,33 @@ public class SQLTest {
 
     @Test
     public void testJoin2() throws Exception {
-        SQL.Select select = SQL.Select("*")
+        Select select = Select("*")
             .From("table1 t1")
             .InnerJoin("Table2 t2 on t1.id=t2.id+?", 10)
             .Where("t2.name=?", "aaa")
             .Where("t2.name=?", "bbb")
-            .Where("t2.name=?", "ccc")
-            ;
+            .Where("t2.name=?", "ccc");
 
         output(select);
     }
 
     @Test
     public void testIn() throws Exception {
-        SQL.Select select = SQL.Select("*")
+        Select select = Select("*")
             .From("table1 t1")
             .Where("name in ?", Arrays.asList("name1", "name2", "name3"))
-            .Or("id in ?", "id1", "id2", "id3")
-            ;
+            .Or("id in ?", "id1", "id2", "id3");
 
         output(select);
+    }
+
+    @Test
+    public void testChildStatements() throws Exception {
+        output(Select("*").From("t1")
+            .Where("col1 in", Select("pid").From("t2").Where("t2.name=?", "aaa"))
+            .And("col2 not in ", Select("qid").From("t3").Where("t3.xxx in ?", "111", "222", "333"))
+            .OrderBy("col3 desc")
+            .Limit(100)
+        );
     }
 }
